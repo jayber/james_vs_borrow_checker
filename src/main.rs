9034dc60d597c5d7f666c8574bef365b23ba9c32
james <james.a.bromley@gmail.com>
2023-01-27 15:13:02 +0000

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use cursive::CursiveRunnable;
+use cursive::views::{EditView, LinearLayout, TextView};
+
+struct MyTree {
+    name: String,
+    entries: Vec<MyTree>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let tree = build_tree();
+
+    let mut siv = cursive::default();
+    siv.add_layer(display(&tree));
+    siv.run();
+}
+
+fn display(tree: &MyTree) -> LinearLayout {
+    let mut layout = LinearLayout::vertical();
+
+    for branch in tree.entries.iter() {
+        let mut field = EditView::new();
+        field = field.on_submit(|x, y| {
+            x.pop_layer();
+            x.add_layer(display(branch));
+        });
+        layout.add_child(field);
+    }
+
+    layout
+}
+
+fn build_tree() -> MyTree {
+    MyTree { name: "test".to_string(), entries: vec![]}
 }
